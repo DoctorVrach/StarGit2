ecd51ce8888e571086c1afa1495ee8b152858943
DoctorVrach <ern.no@mail.ru>
2019-03-17 12:47:02 +0300

    The best commit

--- app/src/main/java/com/example/stargit2/MainActivity.java
+++ app/src/main/java/com/example/stargit2/MainActivity.java
@@ -2,17 +2,32 @@ package com.example.stargit2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.stargit2.R;
+import java.text.BreakIterator;
 
-    Button btn;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    int i = 0;
+    TextView mainTextView;
+    Button mainButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainTextView = findViewById(R.id.main_textview);
+        mainButton = findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+    }
+    @Override
 
-        btn = findViewById(R.id.button);
+    public void onClick(View v) {
+        i=i+1;
+        mainTextView.setText(String.valueOf(i));
     }
 }
+
